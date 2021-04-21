854a574ba4a0028224ec02d1cea2b9c5e9933d5d
Mohammad Sakib Hossain <40665494+shfsakib@users.noreply.github.com>
2021-04-21 18:57:02 +0600

    update password added

---
+++ ChildLearningApp/admin/update-password.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/admin/admin.Master" AutoEventWireup="true" EnableEventValidation="false" CodeBehind="update-password.aspx.cs" Inherits="ChildLearningApp.admin.update_password" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="row">
+        <div class="col-12">
+            <h3>Update Password</h3>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12">
+            <div class="row">
+                <div class="col-md-2"></div>
+                <div class="col-md-2">User List</div>
+                <div class="col-md-6">
+                    <asp:DropDownList ID="ddlUser" class="form-control" runat="server"></asp:DropDownList>
+                </div>
+                <div class="col-md-2"></div>
+            </div>
+            <div class="row">
+                <div class="col-md-2"></div>
+                <div class="col-md-2">New Password</div>
+                <div class="col-md-6">
+                    <asp:TextBox ID="txtnew" class="form-control" placeholder="New Password" TextMode="Password" runat="server"></asp:TextBox>
+                </div>
+                <div class="col-md-2"></div>
+            </div>
+            <div class="row">
+                <div class="col-md-2"></div>
+                <div class="col-md-2">Confirm Password</div>
+                <div class="col-md-6">
+                    <asp:TextBox ID="txtConfirmPass" class="form-control" placeholder="Confirm Password" TextMode="Password" runat="server"></asp:TextBox>
+                </div>
+                <div class="col-md-2"></div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-2"></div>
+                <div class="col-md-2"></div>
+                <div class="col-md-6">
+                    <asp:Button ID="btnUpdate" CssClass="btn btn-primary mt2" Style="margin-top: 5px !important;" runat="server" OnClick="btnUpdate_OnClick" Text="Change Password" />
+                </div>
+                <div class="col-md-2"></div>
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ ChildLearningApp/admin/update-password.aspx.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ChildLearningApp.DAL;
+
+namespace ChildLearningApp.admin
+{
+    public partial class update_password : System.Web.UI.Page
+    {
+        private Function function;
+        private SqlConnection con;
+        private SqlCommand cmd;
+        public update_password()
+        {
+            function=Function.GetInstance();
+            con=new SqlConnection(function.Connection);
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                function.BindDropDown(ddlUser,"select","SELECT Name +' | '+Email Name,id FROM ADMIN ORDER BY Name ASC");
+            }
+        }
+
+        protected void btnUpdate_OnClick(object sender, EventArgs e)
+        {
+            if (ddlUser.SelectedItem.ToString()=="--SELECT--")
+            {
+                function.ShowAlert(this,"Select an user first");
+            }else if (txtnew.Text=="")
+            {
+                function.ShowAlert(this,"New password is required");
+            }else if (txtnew.Text!=txtConfirmPass.Text)
+            {
+                function.ShowAlert(this,"Password mismatch");
+            }
+            else
+            {
+                bool ans = Update();
+                if (ans)
+                {
+                    ddlUser.SelectedIndex = -1;
+                    function.ShowAlert(this,"Password changed successfully");
+                }
+            }
+        }
+        internal bool Update()
+        {
+            bool result = false;
+            SqlTransaction transaction = null;
+            try
+            {
+                if (con.State != ConnectionState.Open)
+                    con.Open();
+                transaction = con.BeginTransaction();
+                cmd = new SqlCommand("UPDATE Admin SET Password=@Password WHERE Id=@Id", con);
+                cmd.Parameters.AddWithValue("@Password", txtnew.Text);
+                cmd.Parameters.AddWithValue("@Id", ddlUser.SelectedValue);
+                cmd.Transaction = transaction;
+                cmd.ExecuteNonQuery();
+                transaction.Commit();
+                result = true;
+                if (con.State != ConnectionState.Closed)
+                    con.Close();
+            }
+            catch (Exception)
+            {
+                transaction.Rollback();
+            }
+            return result;
+        }
+    }
+}
